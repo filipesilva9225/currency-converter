9697fbb98fddd13b1cf6400b9684d42df3513efe
Filipe Silva <140545021+filipesilva9225@users.noreply.github.com>
2025-06-09 21:05:23 +0000

    add all files

---
+++ scripts.js
@@ -0,0 +1,8 @@
+let form = document.getElementById("converterForm");
+
+function convertMoney() {}
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  convertMoney();
+});
